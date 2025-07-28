c7dccdc1ac1fc3a3a1cf332e8eb0a681fbbfb212
Maija Runce <maijarunce11@gmail.com>
2025-07-28 13:48:35 +0300

    v2v21

--- src/21-matrix/index.ts
+++ src/21-matrix/index.ts
@@ -35,14 +35,15 @@
  */
 
 class Matrix {
-  constructor(private matrix: string) {}
-
-  get rows() {
-    return [];
-  }
-
-  get columns() {
-    return [];
+  readonly rows: number[][]
+  readonly columns: number[][]
+  constructor(text: string) {
+    this.rows = text.split('\n').map((row) => {
+      return row.split(/\s+/).map(Number)
+    })
+    this.columns = this.rows[0].map((_, i) => {
+      return this.rows.map((row) => row[i])
+    })
   }
 }
 
