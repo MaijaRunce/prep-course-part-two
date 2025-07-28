0d7d4128a2b32c17f5d8d6a1b34770a92ed2dead
Maija Runce <maijarunce11@gmail.com>
2025-07-28 12:38:16 +0300

    v2v19

--- src/18-pyramid/index.ts
+++ src/18-pyramid/index.ts
@@ -16,6 +16,20 @@
  *              '#####'
  */
 
-function pyramid(n: number) {}
+const pyramid=(n: number) => {
+    for(let i=1; i<= n; i++){
+
+    let str = ' '.repeat(n-i);
+
+    let str2 = '#'. repeat(i*2 -1)
+
+ 
+
+    console.log(str + str2 + str);
+
+  }
+
+}
+
 
 export { pyramid };
